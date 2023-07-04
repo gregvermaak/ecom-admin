e8c66562a446b0b3be41b33a9bf12a6b3581b029
Greg <gregvermaak@icloud.com>
2023-07-04 13:58:32 +1200

    initial commit

---
+++ components/modals/store-modal.tsx
@@ -0,0 +1,30 @@
+"use client"
+
+import * as z from "zod"
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+
+import { Modal } from "@/components/ui/modal"
+import { useStoreModal } from "@/hooks/use-store-modal"
+
+const formSchema = z.object({
+  name: z.string().min(1),
+})
+
+export const StoreModal = () => {
+  const storeModal = useStoreModal()
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+  })
+
+  return (
+    <Modal
+      title="Create store"
+      description="Add a new store to manage products and categories"
+      isOpen={storeModal.isOpen}
+      onClose={storeModal.onClose}>
+      Future Create Store Form
+    </Modal>
+  )
+}
